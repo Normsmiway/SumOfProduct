732dad5596c0b83ca52bd9e8a892671d544614b2
Tajudeen Adenekan <adenekan.adeleke@equitybank.co.ke>
2022-06-13 11:10:11 +0100

    update:other formular

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ConsoleApp1
 {
@@ -8,12 +10,39 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Output: ");
+            Console.WriteLine();
+            Console.WriteLine("Using Some of Product");
             Console.WriteLine(12 * 12 == PrintProduct("12", "12"));
             Console.WriteLine(571 * 12 == PrintProduct("571", "12"));
             Console.WriteLine(5 * 2 == PrintProduct("5", "2"));
             Console.WriteLine(5 * 0 == PrintProduct("5", "0"));
             Console.WriteLine(0 * 10 == PrintProduct("0", "10"));
             Console.WriteLine(3510 * 90220 == PrintProduct("3510", "90220"));
+            Console.WriteLine();
+
+            Console.WriteLine("Using Padded String Formular...");
+            Console.WriteLine(129 * 13 == ProductOfStringPaddedStringFormular("129", "13"));
+            Console.WriteLine(12 * 12 == ProductOfStringPaddedStringFormular("12", "12"));
+            Console.WriteLine(571 * 12 == ProductOfStringPaddedStringFormular("571", "12"));
+            Console.WriteLine(5 * 2 == ProductOfStringPaddedStringFormular("5", "2"));
+            Console.WriteLine(5 * 0 == ProductOfStringPaddedStringFormular("5", "0"));
+            Console.WriteLine(0 * 10 == ProductOfStringPaddedStringFormular("0", "10"));
+            Console.WriteLine(3510 * 90220 == ProductOfStringPaddedStringFormular("3510", "90220"));
+            Console.WriteLine(1510 * 30220 == ProductOfStringPaddedStringFormular("1510", "30220"));
+            Console.WriteLine(415 * 322 == ProductOfStringPaddedStringFormular("322", "415"));
+
+            Console.WriteLine();
+            Console.WriteLine("Using Padded int Formular...");
+            Console.WriteLine(129 * 13 == ProductOfStringPaddedIntFormular("129", "13"));
+            Console.WriteLine(12 * 12 == ProductOfStringPaddedIntFormular("12", "12"));
+            Console.WriteLine(571 * 12 == ProductOfStringPaddedIntFormular("571", "12"));
+            Console.WriteLine(5 * 2 == ProductOfStringPaddedIntFormular("5", "2"));
+            Console.WriteLine(5 * 0 == ProductOfStringPaddedIntFormular("5", "0"));
+            Console.WriteLine(0 * 10 == ProductOfStringPaddedIntFormular("0", "10"));
+            Console.WriteLine(3510 * 90220 == ProductOfStringPaddedIntFormular("3510", "90220"));
+            Console.WriteLine(1510 * 30220 == ProductOfStringPaddedIntFormular("1510", "30220"));
+            Console.WriteLine(415 * 322 == ProductOfStringPaddedStringFormular("322", "415"));
+
             Console.ReadLine();
 
             /*
@@ -45,6 +74,47 @@ namespace ConsoleApp1
                 sum += remainder * int.Parse(y);
             }
             return sum;
+        }//
+
+        static int ProductOfStringPaddedStringFormular(string x, string y)
+        {
+            (x, y) = x.Length > y.Length ? (x, y) : (y, x);
+            int sum = 0;
+            int counter = 0;
+
+            for (int i = y.Length - 1; i >= 0; i--)
+            {
+                int m = int.Parse(y[i].ToString());
+                string product = (int.Parse(x) * m).ToString();
+                product = product.PadRight(product.Length + counter, '0');
+                sum += int.Parse(product);
+                counter += 1;
+            }
+            return sum;
+        }
+
+        static int ProductOfStringPaddedIntFormular(string x, string y)
+        {
+            (x, y) = x.Length > y.Length ? (x, y) : (y, x);
+            int sum = 0;
+            int counter = 0;
+            for (int i = y.Length - 1; i >= 0; i--)
+            {
+                int m = int.Parse(y[i].ToString());
+                int product = (int.Parse(x) * m).PadRight(counter);
+                sum += product;
+                counter += 1;
+            }
+            return sum;
+        }
+    }
+
+    public static class IntExtension
+    {
+        public static int PadRight(this int val, int value)
+        {
+            var pow = Convert.ToInt32(Math.Pow(10, value));
+            return val * pow;
         }
     }
 }
